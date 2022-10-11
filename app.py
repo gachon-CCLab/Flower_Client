486fd4ae4346fcdad91b2ac6ee81a47bffaed2de
Semo <tpah20@gmail.com>
2022-10-12 01:54:46 +0900

    .

--- app.py
+++ app.py
@@ -402,9 +402,8 @@ def load_partition():
     # data check log 생성
     # data_result = {"client_num": {status.FL_client_num}, "data_check": dict_counter}
     # json_data_result = json.dumps(data_result)
-    logging.info('data_check - {"client_num": %s, "label_0": %s, "label_1": %s, \
-    "label_2": %s, "label_3": %s, "label_4": %s, "label_5": %s "label_6": %s, \
-    "label_7": %s, "label_8": %s, "label_9": %s}'%(status.FL_client_num, counter[0], counter[1], counter[2], counter[3], counter[4],
+    logging.info('data_check - {"client_num": %s, "label_0": %s, "label_1": %s, "label_2": %s, "label_3": %s, "label_4": %s, "label_5": %s "label_6": %s, "label_7": %s, "label_8": %s, "label_9": %s}'
+    %(status.FL_client_num, counter[0], counter[1], counter[2], counter[3], counter[4],
     counter[5], counter[6], counter[7], counter[8], counter[9]))
 
     # print(f'client_num: {status.FL_client_num}, data_check: {dict_counter}')
